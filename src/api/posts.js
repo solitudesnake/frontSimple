d254b1be0b79001b098f2614746ac44d0d0903b7
Mauricio Astorga <54522235+solitudesnake@users.noreply.github.com>
2023-02-09 17:58:03 -0300

    Update posts.js
    change in api dir

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:9090/soldiers'
+//     baseURL: 'http://localhost:9090/soldiers'
+    baseURL: 'https://mgsoldiergenserver.onrender.com/soldiers'
 });
